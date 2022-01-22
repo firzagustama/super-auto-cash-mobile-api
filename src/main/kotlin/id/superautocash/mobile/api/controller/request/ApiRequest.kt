a31423ff9bbcfcf4de7bca01aba134b28ee56697
firzagustama <muhfir.za@yahoo.com>
2022-01-23 03:43:00 +0700

    add extParams to request and response

--- src/main/kotlin/id/superautocash/mobile/api/controller/request/ApiRequest.kt
+++ src/main/kotlin/id/superautocash/mobile/api/controller/request/ApiRequest.kt
@@ -1,6 +1,6 @@
 package id.superautocash.mobile.api.controller.request
 
 data class ApiRequest<T>(
-    val token: String? = null,
-    val data: T? = null
+    val data: T? = null,
+    val extParams: MutableMap<String, Any> = mutableMapOf()
 ): BaseRequest()
--- src/main/kotlin/id/superautocash/mobile/api/controller/response/ApiResponse.kt
+++ src/main/kotlin/id/superautocash/mobile/api/controller/response/ApiResponse.kt
@@ -4,5 +4,6 @@ data class ApiResponse<T>(
     var success: Boolean,
     var errorCode: String? = null,
     var errorMessage: String? = null,
-    var data: T?
+    var data: T?,
+    var extParams: MutableMap<String, Any> = mutableMapOf()
 ) : BaseResponse()
